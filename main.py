7fd97e42a5d26b08c46b87332b67d1c5aba6b58c
Logika(Admin) <domalegamarinet@qmail.com>
2024-06-08 18:42:14 +0300

    1

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import pygame
 pygame.init()
 
-screen = pygame.display.set_mode((0, 0),pygame.FULLSCREEN)
+class Section:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x, y, 50, 50)
+
+    def draw(self, scr):
+        pygame.draw.rect(scr, 'white', self.rect, 3)
+
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 running = True
 
 kybiki = []
 for x in range(0,500,50):
     for y in range(0,500,50):
-        r = pygame.Rect( x + 50, y + 50,50,50)
+        r = Section( x + 50, y + 50)
         kybiki.append(r)
+
 kybiki2 = []
 for x in range(0,500,50):
     for y in range(0,500,50):
-        r = pygame.Rect(x + 600,y + 50,50,50)
+        r =Section(x + 600,y + 50)
         kybiki2.append(r)
 
 while running:
@@ -23,11 +31,13 @@ while running:
             running = False
 
     for k in kybiki:
-        pygame.draw.rect(screen,'white',k,5)
+        k.draw(screen)
     for k in kybiki2:
-        pygame.draw.rect(screen,'white',k,5)
+        k.draw(screen)
 
     pygame.display.update()
 
     clock.tick(60)
 pygame.quit()
+
+
---
+++ ships.py
@@ -0,0 +1,15 @@
+class SingleShip:
+    def __init__(self):
+        self.section = 1
+
+class DoubleShip:
+    def __init__(self):
+        self.section = 2
+
+class TripleShip:
+    def __init__(self):
+        self.section = 3
+
+class QuarterShip:
+    def __init__(self):
+        self.section = 4
