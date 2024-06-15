7c4eed98390fdfa7a925b65a3b6bf12442013bb2
mrmrNice <domalegamarinet@gmail.com>
2024-06-15 18:40:40 +0300

    1
    Signed-off-by: Marina <domalegamarinet@gmail.com>

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
+from ships import *
+import random
 pygame.init()
 
 class Section:
     def __init__(self, x, y):
         self.rect = pygame.Rect(x, y, 50, 50)
+        self.color = 'white'
 
     def draw(self, scr):
-        pygame.draw.rect(scr, 'white', self.rect, 3)
+        pygame.draw.rect(scr, self.color, self.rect, 3)
 
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
@@ -21,8 +24,17 @@ for x in range(0,500,50):
 kybiki2 = []
 for x in range(0,500,50):
     for y in range(0,500,50):
-        r =Section(x + 600,y + 50)
+        r = Section(x + 600,y + 50)
         kybiki2.append(r)
+
+ship1 = QuarterShip()
+s = random.choice(kybiki)
+index = kybiki.index(s)
+sections = []
+for i in range(ship1.section):
+    sections.append(kybiki[index + i])
+
+ship1.setShip(sections)
 
 while running:
     screen.fill('black')
--- ships.py
+++ ships.py
@@ -2,14 +2,34 @@ class SingleShip:
     def __init__(self):
         self.section = 1
 
+    def setShip(self, sections):
+        for s in sections:
+            s.color = 'green'
+
+
 class DoubleShip:
     def __init__(self):
         self.section = 2
+
+    def setShip(self, sections):
+        for s in sections:
+            s.color = 'green'
+
 
 class TripleShip:
     def __init__(self):
         self.section = 3
 
+    def setShip(self, sections):
+        for s in sections:
+            s.color = 'green'
+
 class QuarterShip:
     def __init__(self):
         self.section = 4
+
+
+
+    def setShip(self, sections):
+        for s in sections:
+            s.color = 'green'
